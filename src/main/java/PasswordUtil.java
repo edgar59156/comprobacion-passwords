0de952b3af92e48437b7b6efa35a8ab5f6d7266c
Luz <18030636@itcelaya.edu.mx>
2021-05-06 20:34:32 -0500

    creacion clase test

--- src/main/java/PasswordUtil.java
+++ src/main/java/PasswordUtil.java
@@ -3,7 +3,7 @@ public class PasswordUtil {
     public enum SecurityLevel{
         WEAK, MEDIUM,STRONG
     }
-    public SecurityLevel assesPassword(String password){
+    public static SecurityLevel assesPassword(String password){
         return null;
     }
 }
---
+++ src/main/java/PasswordUtilTest.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PasswordUtilTest {
+    @Test
+    public void weak_when_has_less_than_8_letters(){
+
+    }
+
+
+
+}
