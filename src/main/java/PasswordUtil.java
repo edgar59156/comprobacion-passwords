02ac6c47ca6b32d42c1ee2e4799ca18f87fd90db
Luz <18030636@itcelaya.edu.mx>
2021-05-06 20:28:55 -0500

    creación security level

--- src/main/java/PasswordUtil.java
+++ src/main/java/PasswordUtil.java
@@ -1,3 +1,9 @@
 public class PasswordUtil {
 
+    public enum SecurityLevel{
+        WEAK, MEDIUM,STRONG
+    }
+    public SecurityLevel assesPassword(String password){
+        return null;
+    }
 }
